3543f2b4fd98b090e689aa112ceb67c2d4244481
keyum <user.email>
2024-09-29 13:23:10 +0530

    Add existing files

--- controllers/dicsountController.js
+++ controllers/dicsountController.js
@@ -3,7 +3,11 @@ const User = require('../models/user')
 exports.getDiscountAmount = async(req,res,next) =>{
   const pageId = req.get('Authorization')
   console.log(pageId)
+  try{
   const discount = await User.findOne({pageId:pageId})
   console.log(discount)
   res.json(discount.discount)
+  }catch(err){
+    console.log(err)
+  }
 }
