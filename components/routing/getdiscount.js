ff1e6e9883ce40ff04cb599d507357f677f5329b
keyum <user.email>
2024-09-29 11:56:31 +0530

    Add existing files

--- components/routing/getdiscount.js
+++ components/routing/getdiscount.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 const getDiscount = async() => {
   const response = await axios.get('https://chatbot-lovq.onrender.com/discount');
-  console.log(response)
+  console.log(response,response.data,response.body)
 }
 
 module.exports = getDiscount;
